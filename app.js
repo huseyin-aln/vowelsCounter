01c01cb05c809f1363213a0f2dd137be3b584fe5
huseyin <huseyinarslan06@gmail.com>
2022-06-19 00:00:49 +0300

    third step

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
-const inputText = document.getElementById("text");
+const inputText = document.getElementById("input");
 const btnSubmit = document.querySelector(".btn");
 const resultDiv = document.querySelector(".result");
-const span = document.querySelector(".span");
 
-btnSubmit.addEventListener("click", () => {
-    span.innerText = vowelsCounter();
-    inputText.value = "";
-})
 
 const vowelsCounter = () => {
+  let str = inputText.value;
+  let counter = 0;
+str = str.toLowerCase().split("");
 
-    let str = inputText.value;
+str.map((e) => {
+  if (e == "a") {
+    counter += 1;
+  } else if (e == "e") {
+    counter += 1;
+  } else if (e == "i") {
+    counter += 1;
+  } else if (e == "o") {
+    counter += 1;
+  } else if (e == "e") {
+    counter += 1;
+  } else if (e == "u") {
+    counter += 1;
+  }
+});
+return counter;
+};
 
-    let counter = 0;
+btnSubmit.addEventListener("click", () => {
+  resultDiv.innerHTML = `
+        <p class="mt-4">
+          There are <span id="count" class="text-primary fw-bold fs-4">${vowelsCounter()}</span> vowels
+          in <span class="text-danger fw-bold fs-4" id="text">${inputText.value}</span>
+        </p>`
+  inputText.value = "";
+});
 
-  str = str.toLowerCase().split("");
+inputText.addEventListener("keydown", (e) => {
+  if (e.keyCode === 13) {
+    btnSubmit.click();
+  }
+});
 
-  str.map((e) => {
-    if (e == "a") {
-      counter += 1;
-    } else if (e == "e") {
-      counter += 1;
-    } else if (e == "i") {
-      counter += 1;
-    } else if (e == "o") {
-      counter += 1;
-    } else if (e == "e") {
-      counter += 1;
-    } else if (e == "u") {
-      counter += 1;
-    }
-  })
-  return counter;
-}
+window.onload = () => {
+  inputText.focus();
+};
+
+
+
+
